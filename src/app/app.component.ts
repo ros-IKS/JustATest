683f5207231a1b8b41cc1dca83c500e0a4cdef04
ros-IKS <r.schlottmann@iks-gmbh.com>
2022-11-04 16:36:07 +0100

    Kai ist uncool

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
+
+interface InputObject {
+  id: bigint;
+  input: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'JustATest';
+
+  loadedPosts: InputObject[] = [];
+
+  constructor(private httpClient: HttpClient,
+              private formBuilder: FormBuilder) {
+  }
+
+  input = new FormControl('', Validators.required);
+
+  inputForm = this.formBuilder.group({
+    input: this.input
+  })
+
+  onClick() {
+    let url = environment.backendUrl + "/saveInput";
+
+    return this.httpClient.post<any>(url,this.input.value).subscribe(res => {
+      console.log(res);
+    }, err => {
+    })
+  }
+
+  onClick2(){
+    let url = environment.backendUrl + "/fetchInput";
+
+    return this.httpClient.get<InputObject[]>(url).subscribe(res =>{
+      this.loadedPosts = res;
+    })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
